9a665337a2fd9f0a428f3edd9f1c1b80368256cd
Nishant-codx <nimbalkarnishant57@gmail.com>
2026-07-22 23:17:24 +0530

    Add files via upload

---
+++ Exp-03_IMU_Accelerometer/Arduino Sketch/Accelerometer_Serial_Plotter.ino
@@ -0,0 +1,39 @@
+
+
+#include <Arduino_LSM9DS1.h>
+
+void setup() {
+
+  Serial.begin(9600);
+
+  while (!Serial);
+
+  if (!IMU.begin()) {
+
+    Serial.println("Failed to initialize IMU!");
+
+    while (1);
+
+  }
+
+}
+
+void loop() {
+
+  float x, y, z;
+
+  if (IMU.accelerationAvailable()) {
+
+    IMU.readAcceleration(x, y, z);
+
+    Serial.print(x);
+    Serial.print('\t');
+
+    Serial.print(y);
+    Serial.print('\t');
+
+    Serial.println(z);
+
+  }
+
+}
